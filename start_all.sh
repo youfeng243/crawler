e9fda1d4ddeff5117473a40ae0e3da371ced996f
youfeng <youfeng@haizhi.com>
2017-10-12 19:11:53 +0800

    修改启动脚本

--- start_all.sh
+++ start_all.sh
@@ -3,7 +3,7 @@ BIN_PATH=`dirname $0`
 cd ${BIN_PATH}
 sh env.sh
 
-for d in `echo i_downloader   i_extractor   i_scheduler  i_data_saver i_entity_extractor i_manager i_crawler_merge`
+for d in `echo i_downloader   i_extractor   i_scheduler single_src_merger  i_data_saver i_entity_extractor i_manager i_crawler_merge`
 do
     cd $d
         sh run.sh start
--- stop_all.sh
+++ stop_all.sh
@@ -3,7 +3,7 @@ BIN_PATH=`dirname $0`
 cd ${BIN_PATH}
 sh env.sh
 
-for d in `echo i_scheduler i_downloader   i_extractor i_entity_extractor i_data_saver i_crawler_merge i_manager`
+for d in `echo i_scheduler i_downloader  single_src_merger  i_extractor i_entity_extractor i_data_saver i_crawler_merge i_manager`
 do
     cd $d
         sh run.sh stop
