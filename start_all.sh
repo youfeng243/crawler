655aa1cf1e1157510e726a9e762b8d3261efc338
youfeng <youfeng@haizhi.com>
2017-10-12 10:09:39 +0800

    代码调整

--- start_all.sh
+++ start_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BIN_PATH=`dirname $0`
-cd $BIN_PATH
+cd ${BIN_PATH}
 sh env.sh
 
 for d in `echo i_downloader   i_extractor   i_scheduler  i_data_saver i_entity_extractor i_manager i_crawler_merge`
--- stop_all.sh
+++ stop_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BIN_PATH=`dirname $0`
-cd $BIN_PATH
+cd ${BIN_PATH}
 sh env.sh
 
 for d in `echo i_scheduler i_downloader   i_extractor i_entity_extractor i_data_saver i_crawler_merge i_manager`
